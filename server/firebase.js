e16f6be69ffa4c398d126792496da8a8c34e2f3c
Julius Diaz Panoriñgan <jdp@android-4d1219099678490c.localdomain>
2018-09-14 12:58:54 -0700

    implement (temporary) new list generation on nonexistent list id, with subsequent redirect

--- server/firebase.js
+++ server/firebase.js
@@ -1,5 +1,6 @@
-const fbAdmin = require("firebase-admin");
+const path = require("path");
 
+const fbAdmin = require("firebase-admin");
 const fbServiceAccount = require("./_private/todo-41643-firebase-adminsdk-ftoej-d08f538a31.json");
 
 
@@ -11,8 +12,53 @@ const fbApp = fbAdmin.initializeApp({
 fbFirestore = fbApp.firestore();
 fbFirestore.settings( { timestampsInSnapshots: true, } );
 
+function createListAndRedirect(req, res, next) {
+  const newList = fbFirestore.collection("lists").doc()
+  const newListID = newList.id;
+  let newListBatch = fbFirestore.batch();
+
+  newListBatch.set(newList, { title: "To-do list" });
+  for(let i=0; i<3; i++){
+    let newListItem = fbFirestore.collection(`lists/${newListID}/todos`).doc();
+    newListBatch.set(
+      newListItem, 
+      { 
+        title: `To-do item #${i+1}`, 
+        desc: "To-do description",
+        time: fbAdmin.firestore.FieldValue.serverTimestamp(),
+      }
+    );
+  }
+  
+  newListBatch.commit()
+  .then(() => {
+    res.redirect(`/${newListID}`);
+  })
+  .catch(() => {
+    next(new Error("firestore document set failed"));
+  });
+}
+
+function serveList(req, res, next) {
+  
+  fbFirestore.doc(`lists/${req.params.listID}`).get()
+    .then( (snapshot) => {
+      const todoList = snapshot.data();
+      if(todoList!==undefined){
+        res.sendFile(path.join(__dirname, "public", "index.html"));
+      }else{
+        res.redirect("/");
+      }
+    })
+    .catch(() => {
+      next(new Error("firestore document get failed"));
+    });
+  //res.sendFile(path.join(__dirname, "public", "index.html"));
+}
 
 module.exports = {
-  firestore: fbFirestore,
-  FieldValue: fbAdmin.firestore.FieldValue,
+  //firestore: fbFirestore,
+  //FieldValue: fbAdmin.firestore.FieldValue,
+  createListAndRedirect,
+  serveList,
 };
--- server/router.js
+++ server/router.js
@@ -2,50 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 
-const { firestore, FieldValue } = require("./firebase");
+const { 
+  createListAndRedirect, 
+  serveList 
+} = require("./firebase");
 
-router.get("/", function(req, res, next) {
-  const newList = firestore.collection("lists").doc()
-  const newListID = newList.id;
-  let newListBatch = firestore.batch();
-
-  newListBatch.set(newList, { title: "To-do list" });
-  for(let i=0; i<3; i++){
-    let newListItem = firestore.collection(`lists/${newListID}/todos`).doc();
-    newListBatch.set(
-      newListItem, 
-      { 
-        title: "To-do item", 
-        desc: "To-do description",
-        time: FieldValue.serverTimestamp(),
-      }
-    );
-  }
-  
-  newListBatch.commit()
-  .then(() => {
-    res.redirect(`/${newListID}`);
-  })
-  .catch(() => {
-    next(new Error("firestore document set failed"));
-  });
-});
-
-router.get("/:listID", function(req, res, next) {
-  //res.send(req.params.listID);
-  firestore.doc(`lists/${req.params.listID}`).get()
-    .then( (snapshot) => {
-      const todoList = snapshot.data();
-      if(todoList!==undefined){
-        res.json(todoList);
-      }else{
-        next(new Error("to-do list does not exist"));
-      }
-    })
-    .catch(() => {
-      next(new Error("firestore document get failed"));
-    });
-});
+router.get("/", createListAndRedirect);
+router.get("/:listID", serveList);
 
 
 module.exports = router;
